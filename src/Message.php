96fb25947d30b6dac58c87638e09799870a4841c
Ludovic Leroux <ludovic.leroux@corp.ovh.com>
2016-07-13 16:31:30 +0200

    Fix the message coding detection
    Signed-off-by: Ludovic Leroux <ludovic.leroux@corp.ovh.com>

--- src/Message.php
+++ src/Message.php
@@ -153,7 +153,7 @@ class Message
         }
 
         // Manage coding
-        $coding = (max(array_map('ord', str_split($message))) > 127) ? '8bit' : '7bit';
+        $coding = ($this->is_gsm0338($message) ? '7bit' : '8bit');
 
         // Prepare request parameters
         $parameters = (object) array('message' => $message, 'receivers' => $this->receivers, 'noStopClause' => !$this->isMarketing, 'differedPeriod' => $differedPeriod, 'coding' => $coding, 'tag' => $this->tag);
@@ -224,5 +224,37 @@ class Message
         }
 
         $this->sender = $sender;
+    }
+
+    private function is_gsm0338($utf8_string) {
+
+        $gsm0338 = array(
+            '@','Δ',' ','0','¡','P','¿','p',
+            '£','_','!','1','A','Q','a','q',
+            '$','Φ','"','2','B','R','b','r',
+            '¥','Γ','#','3','C','S','c','s',
+            'è','Λ','¤','4','D','T','d','t',
+            'é','Ω','%','5','E','U','e','u',
+            'ù','Π','&','6','F','V','f','v',
+            'ì','Ψ','\'','7','G','W','g','w',
+            'ò','Σ','(','8','H','X','h','x',
+            'Ç','Θ',')','9','I','Y','i','y',
+            "\n",'Ξ','*',':','J','Z','j','z',
+            'Ø',"\x1B",'+',';','K','Ä','k','ä',
+            'ø','Æ',',','<','L','Ö','l','ö',
+            "\r",'æ','-','=','M','Ñ','m','ñ',
+            'Å','ß','.','>','N','Ü','n','ü',
+            'å','É','/','?','O','§','o','à'
+        );
+
+        $len = mb_strlen($utf8_string, 'UTF-8');
+
+        for( $i=0; $i < $len; $i++) {
+            if (!in_array(mb_substr($utf8_string,$i,1,'UTF-8'), $gsm0338)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
